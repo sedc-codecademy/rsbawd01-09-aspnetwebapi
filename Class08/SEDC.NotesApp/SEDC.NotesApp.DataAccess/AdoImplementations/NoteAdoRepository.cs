b5f91bd45902bd277f03ee522d6efbe4b00ccca6
Almir Vuk <almir.vuk@outlook.com>
2023-11-07 17:23:18 +0100

    Class 08 code added.

---
+++ Class08/SEDC.NotesApp/SEDC.NotesApp.DataAccess/AdoImplementations/NoteAdoRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using SEDC.NotesApp.Domain.Enums;
+using SEDC.NotesApp.Domain.Models;
+
+namespace SEDC.NotesApp.DataAccess.AdoImplementations
+{
+    public class NoteAdoRepository : IRepository<Note>
+    {
+        private string _connectionString;
+
+        public NoteAdoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Add(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Note> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Class08/SEDC.NotesApp/SEDC.NotesApp.DataAccess/DapperImplementations/NoteDapperRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SEDC.NotesApp.Domain.Models;
+
+namespace SEDC.NotesApp.DataAccess.DapperImplementations
+{
+    public class NoteDapperRepository : IRepository<Note>
+    {
+        private string _connectionString;
+
+        public NoteDapperRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Add(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Note> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Note entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
